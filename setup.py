0f17a3598fab2a3263344fcd605cb544acc05adc
Subhrojeet Dutta <106841956+geeksubhro@users.noreply.github.com>
2024-04-17 19:35:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+
+
+setup(
+    name = "AgriculturePipeline", 
+    version = "1.0.0.", 
+    author = "Subhrojeet Dutta", 
+    description= "A Prediction Pipeline for Agriculture Data.",
+    author_email= "Subhrojeet.mail@gmail.com", 
+    packages=find_packages(),
+    )
